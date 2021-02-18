46b5d280ced30024d9e653a6284fd2840c7f54dc
Un-Jarvis <huang.2089@osu.edu>
2021-02-18 16:00:43 -0500

    Removed coins and checkboxes

--- sketch.js
+++ sketch.js
@@ -11,20 +11,8 @@ let warnColor;
 let player;
 let enemies = [];
 let enemiesNumber = 3;
-let coins = [];
-let coinsNumber = 10;
 let bullets = [];
 let bulletIntervalId = 0;
-
-// checkboxes
-let coinAnimated;
-let coinAnimatedBool = true;
-let screenShake;
-let screenShakeBool = true;
-let recoil;
-let recoilBool = true;
-let warning;
-let warningBool = true;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
@@ -36,16 +24,6 @@ function setup() {
   // set up objects
   player = new Player();
 
-  // game settings
-  coinAnimated = createCheckbox("Coin Animated", true);
-  coinAnimated.changed(coinAnimatedEvent);
-  screenShake = createCheckbox("Screenshake", true);
-  screenShake.changed(screenShakeEvent);
-  recoil = createCheckbox("Recoil", true);
-  recoil.changed(recoilEvent);
-  warning = createCheckbox("Warning", true);
-  warning.changed(warningEvent);
-
   noStroke();
   rectMode(CENTER);
 }
@@ -53,16 +31,6 @@ function setup() {
 function draw() {
   // set up environment
   background(245);
-
-  // screen shake
-  if (screenShakeBool && attacked) {
-    translate(random(-10, 10), random(-10, 10));
-  }
-
-  // warn
-  if (warningBool && attacked) {
-    warningDisplay();
-  }
 
   if (attacked) {
     scoreColor = color(255, 0, 0);
@@ -75,21 +43,12 @@ function draw() {
   if (enemies.length < enemiesNumber) {
     enemies.push(new Enemy());
   }
-  if (coins.length < coinsNumber) {
-    coins.push(new Coin());
-  }
 
   // move objects
   player.move();
   for (let enemy of enemies) {
     enemy.move();
     enemy.display();
-  }
-  for (let coin of coins) {
-    if (coinAnimatedBool) {
-      coin.move();
-    }
-    coin.display();
   }
   for (let i = 0; i < bullets.length; i++) {
     let bullet = bullets[i];
@@ -105,9 +64,6 @@ function draw() {
       bullets.splice(i, 1);
     }
   }
-
-  // reset coins when all coins are collected
-  resetCoins();
 
   // detect collisions
   detectCollision();
@@ -128,13 +84,6 @@ function draw() {
 function mousePressed() {
   bulletIntervalId = setInterval(function () {
     bullets.push(new Bullet(player.x, player.y, mouseX, mouseY));
-
-    if (recoilBool) {
-      let target = createVector(mouseX, mouseY);
-      let playerPos = createVector(player.x, player.y);
-      let direction = playerPos.sub(target);
-      player.recoil(direction);
-    }
   }, 50);
 }
 
@@ -163,69 +112,4 @@ function detectCollision() {
       }
     }
   }
-
-  for (let coin of coins) {
-    if (
-      !coin.destroyed &&
-      !coin.consumed &&
-      dist(coin.x, coin.y, player.x, player.y) <=
-        player.size / 2 + coin.size / 2
-    ) {
-      player.collect();
-
-      coin.consumed = true;
-
-      if (coinAnimatedBool) {
-        coin.velocity = -15;
-        // generate a new coin
-        setTimeout(function () {
-          coin.destroyed = true;
-          coin.consumed = false;
-          coin.x = random(50, canvasWidth - 50);
-          coin.y = random(50, canvasHeight - 50);
-        }, 1000);
-      } else {
-        coin.destroyed = true;
-        coin.consumed = false;
-        coin.x = random(50, canvasWidth - 50);
-        coin.y = random(50, canvasHeight - 50);
-      }
-    }
-  }
-}
-
-function resetCoins() {
-  let allConsumed = true;
-  for (let coin of coins) {
-    if (!coin.destroyed) {
-      allConsumed = false;
-    }
-  }
-  if (allConsumed) {
-    for (let coin of coins) {
-      coin.destroyed = false;
-    }
-  }
-}
-
-function warningDisplay() {
-  warnColor.setAlpha(25 + 25 * sin(millis() / 300));
-  fill(warnColor);
-  rect(canvasWidth / 2, canvasHeight / 2, canvasWidth, canvasHeight);
-}
-
-function coinAnimatedEvent() {
-  coinAnimatedBool = this.checked();
-}
-
-function screenShakeEvent() {
-  screenShakeBool = this.checked();
-}
-
-function recoilEvent() {
-  recoilBool = this.checked();
-}
-
-function warningEvent() {
-  warningBool = this.checked();
 }
